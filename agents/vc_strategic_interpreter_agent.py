a7d459c4e13b636f5666e284e8b649cffe55705a
dcamodeo1983 <dcamodeo@gmail.com>
2025-04-24 12:33:41 -0400

    Update vc_strategic_interpreter_agent.py

--- agents/vc_strategic_interpreter_agent.py
+++ agents/vc_strategic_interpreter_agent.py
@@ -28,7 +28,7 @@ Portfolio Companies:
 {formatted_entries[:3000]}
 """
             response = self.client.chat.completions.create(
-                model="gpt-4",
+                model="gpt-3.5-turbo"
                 messages=[{"role": "user", "content": prompt}],
                 temperature=0.4,
                 max_tokens=750
